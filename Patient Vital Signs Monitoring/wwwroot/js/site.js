59ebc4e26b999ab0d1e1dd2769ca9188b45fde48
alex varypatis <alexvarip@windowslive.com>
2025-08-03 03:36:16 +0300

    Add Bootstrap tooltip initialization on DOMContentLoaded

--- Patient Vital Signs Monitoring/wwwroot/js/site.js
+++ Patient Vital Signs Monitoring/wwwroot/js/site.js
@@ -1,4 +1,13 @@
 ﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Write your JavaScript code.
+document.addEventListener('DOMContentLoaded', function () {
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+    tooltipTriggerList.forEach(function (tooltipTriggerEl) {
+        new bootstrap.Tooltip(tooltipTriggerEl, {
+            delay: { "show": 500, "hide": 100 },
+            placement: 'bottom',               
+            animation: true
+        });
+    });
+});
