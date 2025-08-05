aca207e6ebb52990d573b0c150db4630b8494027
alex varypatis <alexvarip@windowslive.com>
2025-08-05 23:29:03 +0300

    Removed comments

--- Patient Vital Signs Monitoring/Pages/MonitorVitals.cshtml.cs
+++ Patient Vital Signs Monitoring/Pages/MonitorVitals.cshtml.cs
@@ -31,9 +31,7 @@ namespace Patient_Vital_Signs_Monitoring.Pages
         [BindProperty(SupportsGet = true)]
         public Guid Id { get; set; }
 
-        // TODO : Add Historical data view for the last 24 hours in monitoring mode
-        //        as Progressive Line With Easing in chart.js
-
+    
         public void OnGet()
         {
             signs = _repository.GetAllVitalSignsAsync(Id).Result
